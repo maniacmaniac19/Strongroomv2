824dd45b42ab67d132c98541c8e46285df5ce673
Jared Smith <jsmith1088@gmail.com>
2018-12-02 19:28:43 -0600

    heroku test

---
+++ client/build/precache-manifest.acd7f964cc35a5c38b7916cc3beaa47f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5341756d5db896359473",
+    "url": "/static/js/main.5341756d.chunk.js"
+  },
+  {
+    "revision": "b95cd2b2439a55ec7cdc",
+    "url": "/static/js/1.b95cd2b2.chunk.js"
+  },
+  {
+    "revision": "5341756d5db896359473",
+    "url": "/static/css/main.7875289a.chunk.css"
+  },
+  {
+    "revision": "2078f3fc30adbe596fe3ae60dd6bd20d",
+    "url": "/index.html"
+  }
+];
